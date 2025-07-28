b6a6a89fdcda4fc538162b791a82dd0e261e3446
Carlos Eduardo Menegassi <161506037+CarlosEM007@users.noreply.github.com>
2025-07-28 20:02:58 -0300

    Feat: Criado o projeto, adicionado o player e uma movimentação, além, das animações.

--- Assets/Scripts/JogadorScript.cs
+++ Assets/Scripts/JogadorScript.cs
@@ -2,15 +2,68 @@ using UnityEngine;
 
 public class JogadorScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    #region :: Atributos ::
+    [SerializeField]
+    private float Velocidade;
+
+    private Rigidbody2D Rigid;
+
+    private Animator AnimacaoMovimento;
+
+    private string AnimacaoAuxiliar;
+
+    #endregion :: Atributos ::
+
     void Start()
     {
-        
+        Rigid = GetComponent<Rigidbody2D>();
+        AnimacaoMovimento = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movimentacao();
     }
+
+    #region :: Eventos ::
+
+    #endregion :: Eventos ::
+
+    #region :: Métodos ::
+
+    void Movimentacao()
+    {
+        float MoveX = Input.GetAxisRaw("Horizontal");
+        float MoveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDir = new Vector2(MoveX, MoveY).normalized;
+        Rigid.MovePosition(Rigid.position + moveDir * Velocidade * Time.deltaTime);
+
+        AnimacaoMovimento.SetBool("LadoWalk", false);
+        AnimacaoMovimento.SetBool("CimaWalk", false);
+        AnimacaoMovimento.SetBool("BaixoWalk", false);
+
+        if (MoveX > 0)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f);
+            AnimacaoMovimento.SetBool("LadoWalk", true);
+        }
+        else if (MoveX < 0)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f);
+            AnimacaoMovimento.SetBool("LadoWalk", true);
+        }
+        else if (MoveY > 0)
+        {
+            AnimacaoMovimento.SetBool("CimaWalk", true);
+        }
+        else if (MoveY < 0)
+        {
+            AnimacaoMovimento.SetBool("BaixoWalk", true);
+        }
+    }
+
+
+
+    #endregion :: Métodos ::
 }
