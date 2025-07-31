f8cb8cfd260b9c31c4021f4eb65f97577ac4e6de
Carlos Eduardo Menegassi <161506037+CarlosEM007@users.noreply.github.com>
2025-07-31 13:19:47 -0300

    feat: Criado sistema de coleta de itens, faltam alguns detalhes mais estéticos.

---
+++ Assets/Scripts/ColetaveisScript.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+public class ColetaveisScript : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] Sprites; // Com recurso, sem recurso.
+
+    [SerializeField]
+    private string Recurso; // Representa o recurso do objeto que receber esse script
+
+    [SerializeField]
+    private int[] Quantidade; // Reprensenta o quanto de itens esse recurso pode droppar.
+
+    [SerializeField]
+    private RecursosScript RecursosControlador;
+
+    private SpriteRenderer Renderer;
+
+    private bool Coletavel;
+ 
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+        Coletavel = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            if (Coletavel && Input.GetKeyDown(KeyCode.E))
+            {
+                Renderer.sprite = Sprites[1];
+                Coletavel = false;
+                RecursosControlador.RecursoColetado(Recurso, QuantidadeColetada());
+            }
+        }
+    }
+
+    private int QuantidadeColetada()
+    {
+        return UnityEngine.Random.Range(Quantidade[0], Quantidade[1]);
+    }
+}
---
+++ Assets/Scripts/RecursosScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class RecursosScript : MonoBehaviour
+{
+    // Conforme o jogo for crescendo, basta adicionar um novo recurso aqui
+    [SerializeField]
+    private int Frutas;
+
+    [SerializeField] 
+    private int Madeira;
+
+    [SerializeField]
+    private int Ferro;
+
+    public void RecursoColetado(string pNomeRecurso, int pQuantidade = 1)
+    {
+        switch (pNomeRecurso)
+        {
+            case "Frutas":
+                Frutas += pQuantidade;
+                break;
+            case "Madeira":
+                Madeira += pQuantidade;
+                break;
+            case "Ferro":
+                Ferro += pQuantidade;
+                break;
+        }
+    }
+}
